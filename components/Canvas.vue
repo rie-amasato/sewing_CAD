2e8d64057e50695c7c9d048e9da534ab7f0cad0e
Rie Amasato <51439946+rie-amasato@users.noreply.github.com>
2025-04-23 06:42:48 +0000

    SVGの仮置き。インターフェースの考慮が必要

---
+++ components/Canvas.vue
@@ -0,0 +1,19 @@
+<template>
+  canvas
+  {{ paths }}
+  <svg
+    width="200"
+    height="100"
+    viewbox="0 0 200 100"
+    fill="#ffaaaa"
+    stroke-width="3"
+  >
+    <path v-for="path in paths" :d="path" stroke="blue"></path>
+  </svg>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  paths: string[];
+}>();
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
   <Menubar />
   <div class="container red grid">
-    <div class="container white s1 e4">Canvas</div>
+    <div class="container white s1 e4">
+      <Canvas :paths="svgpaths" />
+    </div>
     <div class="container white s4 e5">要素りすと</div>
   </div>
 </template>
+
+<script setup lang="ts">
+const svgpaths = ref<string[]>([
+  "M 5 10 L 10 15 L 20 10 L 5 10",
+  "M 5 20 L 10 35 L 90 15 L 20 100 L 5 20",
+]);
+</script>
